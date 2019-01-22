eeb023a7f57cfc617f4c9bec50a5ff6cf51a7ccc
whitequark <whitequark@whitequark.org>
2019-01-22 06:56:46 +0000

    compat.genlib.fifo: adjust _FIFOInterface shim to not require fwft=.

--- nmigen/compat/genlib/fifo.py
+++ nmigen/compat/genlib/fifo.py
@@ -1,5 +1,16 @@
-from ...lib.fifo import FIFOInterface as _FIFOInterface, \
-  SyncFIFO, SyncFIFOBuffered
+from ...tools import deprecated
+from ...lib.fifo import FIFOInterface as NativeFIFOInterface, \
+  SyncFIFO, SyncFIFOBuffered, AsyncFIFO, AsyncFIFOBuffered
 
 
-__all__ = ["_FIFOInterface", "SyncFIFO", "SyncFIFOBuffered"]
+__all__ = ["_FIFOInterface", "SyncFIFO", "SyncFIFOBuffered", "AsyncFIFO", "AsyncFIFOBuffered"]
+
+
+@deprecated("attribute `fwft` must be provided to FIFOInterface constructor")
+class CompatFIFOInterface(NativeFIFOInterface):
+    def __init__(self, width, depth):
+        super().__init__(width, depth, fwft=False)
+        del self.fwft
+
+
+_FIFOInterface = CompatFIFOInterface
