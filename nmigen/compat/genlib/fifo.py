2fb85a61702a1046fd216ba46cfa1b5e9ef586e1
whitequark <whitequark@whitequark.org>
2019-01-26 18:23:58 +0000

    compat.fifo: fix _FIFOInterface deprecation wrapper.

--- nmigen/compat/genlib/fifo.py
+++ nmigen/compat/genlib/fifo.py
@@ -6,8 +6,8 @@ from ...lib.fifo import FIFOInterface as NativeFIFOInterface, \
 __all__ = ["_FIFOInterface", "SyncFIFO", "SyncFIFOBuffered", "AsyncFIFO", "AsyncFIFOBuffered"]
 
 
-@deprecated("attribute `fwft` must be provided to FIFOInterface constructor")
 class CompatFIFOInterface(NativeFIFOInterface):
+    @deprecated("attribute `fwft` must be provided to FIFOInterface constructor")
     def __init__(self, width, depth):
         super().__init__(width, depth, fwft=False)
         del self.fwft
