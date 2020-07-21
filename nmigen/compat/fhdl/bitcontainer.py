0899ff366b5fa7cbfb71da3e6c24bf458fae10ee
whitequark <whitequark@whitequark.org>
2020-07-21 02:53:29 +0000

    compat.fhdl.bitcontainer: fix value_bits_sign().
    This function was broken in commit 659b0e81; some downstream code
    expects bits_sign to be e.g. indexable.

--- nmigen/compat/fhdl/bitcontainer.py
+++ nmigen/compat/fhdl/bitcontainer.py
@@ -18,4 +18,4 @@ def bits_for(n, require_sign_bit=False):
 
 @deprecated("instead of `value_bits_sign(v)`, use `v.shape()`")
 def value_bits_sign(v):
-    return ast.Value.cast(v).shape()
+    return tuple(ast.Value.cast(v).shape())
