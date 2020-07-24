30176ab61c4a25eb73c6bcd5e4b1af452431d381
Jean THOMAS <git0@pub.jeanthomas.me>
2020-07-24 12:27:34 +0200

    nmigen.compat.genlib: Use RoundRobin implementation from oMigen

--- nmigen/compat/genlib/roundrobin.py
+++ nmigen/compat/genlib/roundrobin.py
@@ -1,5 +1,5 @@
 from ..._utils import deprecated
-from ...lib.roundrobin import RoundRobin as NativeRoundRobin
+from ..fhdl.module import CompatModule
 
 
 __all__ = ["RoundRobin", "SP_WITHDRAW", "SP_CE"]
@@ -10,13 +10,41 @@ __all__ = ["RoundRobin", "SP_WITHDRAW", "SP_CE"]
             "use `nmigen.lib.roundrobin.RoundRobin`; note that RoundRobin does not "
             "require a policy anymore, and that the `ce` attribute has been renamed"
             "to `en`")
-class CompatRoundRobin(NativeRoundRobin):
-    def __init__(self, n, switching_policy=SP_WITHDRAW):
-        super().__init__(n)
+class CompatRoundRobin(CompatModule):
+    def __init__(self, n, switch_policy=SP_WITHDRAW):
+        self.request = Signal(n)
+        self.grant = Signal(max=max(2, n))
+        self.switch_policy = switch_policy
+        if self.switch_policy == SP_CE:
+            self.ce = Signal()
 
-        if switching_policy == SP_CE:
-        	self.ce = Signal()
-        	self.comb += self.en.eq(self.ce)
+        ###
+
+        if n > 1:
+            cases = {}
+            for i in range(n):
+                switch = []
+                for j in reversed(range(i+1, i+n)):
+                    t = j % n
+                    switch = [
+                        If(self.request[t],
+                            self.grant.eq(t)
+                        ).Else(
+                            *switch
+                        )
+                    ]
+                if self.switch_policy == SP_WITHDRAW:
+                    case = [If(~self.request[i], *switch)]
+                else:
+                    case = switch
+                cases[i] = case
+            statement = Case(self.grant, cases)
+            if self.switch_policy == SP_CE:
+                statement = If(self.ce, statement)
+            self.sync += statement
+        else:
+            self.comb += self.grant.eq(0)
+
 
 
 RoundRobin = CompatRoundRobin
