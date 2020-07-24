bb2fafd5bef57294e616230717f95a29bf3764e1
Jean THOMAS <git0@pub.jeanthomas.me>
2020-07-24 13:58:20 +0200

    Revamp RoundRobin interface and tests, add documentation

--- nmigen/lib/roundrobin.py
+++ nmigen/lib/roundrobin.py
@@ -5,23 +5,45 @@ __all__ = ["RoundRobin"]
 
 
 class RoundRobin(Elaboratable):
-    def __init__(self, n):
-        self.requests = Signal(n)
-        self.grant = Signal(range(n))
-        self.en = Signal(reset=1)
+    """Round-robin scheduler.
+
+    For a given set of requests, the round-robin scheduler will
+    grant one request.
+
+    Parameters
+    ----------
+    width : int
+        Number of requests.
+
+    Attributes
+    ----------
+    requests : Signal(width), in
+        Set of requests.
+    grant : Signal(range(width)), out
+        Number of the granted request.
+    en : Signal, in
+        Enable signal. Optional.
+    """
+    def __init__(self, *, width):
+        if not isinstance(width, int) or width < 0:
+            raise ValueError("Width must be a positive integer, not {!r}"
+                             .format(width))
+        self.width    = width
+
+        self.requests = Signal(width)
+        self.grant    = Signal(range(width))
+        self.en       = Signal(reset=1)
 
     def elaborate(self, platform):
         m = Module()
 
-        n = self.requests.width
-
         with m.Switch(self.grant):
-            for i in range(n):
+            for i in range(self.width):
                 with m.Case(i):
                     for pred in reversed(range(i)):
                         with m.If(self.requests[pred]):
                             m.d.sync += self.grant.eq(pred)
-                    for succ in reversed(range(i + 1, n)):
+                    for succ in reversed(range(i + 1, self.width)):
                         with m.If(self.requests[succ]):
                             m.d.sync += self.grant.eq(succ)
 
--- nmigen/test/test_lib_roundrobin.py
+++ nmigen/test/test_lib_roundrobin.py
@@ -8,27 +8,42 @@ from ..lib.roundrobin import *
 class RoundRobinSimulationTestCase(unittest.TestCase):
     def test_transitions(self):
         m = Module()
-        m.submodules.dut = dut = RoundRobin(3)
+        m.submodules.dut = dut = RoundRobin(width=3)
 
         sim = Simulator(m)
         def process():
-            yield dut.requests.eq(0b11)
+            yield dut.requests.eq(0b111)
             yield Tick(); yield Delay(1e-8)
             self.assertEqual((yield dut.grant), 1)
 
-            yield dut.requests.eq(0b01)
+            yield dut.requests.eq(0b110)
             yield Tick(); yield Delay(1e-8)
-            self.assertEqual((yield dut.grant), 0)
+            self.assertEqual((yield dut.grant), 2)
 
-            yield dut.requests.eq(0b01)
-            yield Tick(); yield Delay(1e-8)
-            self.assertEqual((yield dut.grant), 0)
-
-            yield dut.requests.eq(0b10)
+            yield dut.requests.eq(0b010)
             yield Tick(); yield Delay(1e-8)
             self.assertEqual((yield dut.grant), 1)
 
-            yield dut.requests.eq(0b01)
+            yield dut.requests.eq(0b011)
+            yield Tick(); yield Delay(1e-8)
+            self.assertEqual((yield dut.grant), 0)
+
+            yield dut.requests.eq(0b001)
+            yield Tick(); yield Delay(1e-8)
+            self.assertEqual((yield dut.grant), 0)
+
+            yield dut.requests.eq(0b101)
+            yield Tick(); yield Delay(1e-8)
+            self.assertEqual((yield dut.grant), 2)
+
+            yield dut.requests.eq(0b100)
+            yield Tick(); yield Delay(1e-8)
+            self.assertEqual((yield dut.grant), 2)
+
+            yield dut.requests.eq(0b000)
+            yield Tick(); yield Delay(1e-8)
+
+            yield dut.requests.eq(0b001)
             yield Tick(); yield Delay(1e-8)
             self.assertEqual((yield dut.grant), 0)
         sim.add_process(process)
