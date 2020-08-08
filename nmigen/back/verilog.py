b8c82c409664ef5d780a0e52b462daf89ad52e4a
Peder Bergebakken Sundt <pbsds@hotmail.com>
2020-08-08 14:37:55 +0200

    back.verilog: refactor out 'write_verilog' from _convert_rtlil_text to enable reuse
    This change enables adding more yosys backends

--- nmigen/back/verilog.py
+++ nmigen/back/verilog.py
@@ -5,7 +5,7 @@ from . import rtlil
 __all__ = ["YosysError", "convert", "convert_fragment"]
 
 
-def _convert_rtlil_text(rtlil_text, *, strip_internal_attrs=False, write_verilog_opts=()):
+def _convert_rtlil_text(rtlil_text, *, strip_internal_attrs=False, write_script=[]):
     # this version requirement needs to be synchronized with the one in setup.py!
     yosys = find_yosys(lambda ver: ver >= (0, 9))
     yosys_version = yosys.version()
@@ -33,7 +33,7 @@ def _convert_rtlil_text(rtlil_text, *, strip_internal_attrs=False, write_verilog
         script.append("attrmap {}".format(" ".join(attr_map)))
         script.append("attrmap -modattr {}".format(" ".join(attr_map)))
 
-    script.append("write_verilog -norename {}".format(" ".join(write_verilog_opts)))
+    script.extend(write_script)
 
     return yosys.run(["-q", "-"], "\n".join(script),
         # At the moment, Yosys always shows a warning indicating that not all processes can be
@@ -42,11 +42,19 @@ def _convert_rtlil_text(rtlil_text, *, strip_internal_attrs=False, write_verilog
         ignore_warnings=True)
 
 
+def _convert_rtlil_text_verilog(rtlil_text, *, strip_internal_attrs=False, write_verilog_opts=()):
+    write_script = []
+    write_script.append("write_verilog -norename {}".format(" ".join(write_verilog_opts)))
+
+    return _convert_rtlil_text(rtlil_text,
+        strip_internal_attrs=strip_internal_attrs, write_script=write_script)
+
+
 def convert_fragment(*args, strip_internal_attrs=False, **kwargs):
     rtlil_text, name_map = rtlil.convert_fragment(*args, **kwargs)
-    return _convert_rtlil_text(rtlil_text, strip_internal_attrs=strip_internal_attrs), name_map
+    return _convert_rtlil_text_verilog(rtlil_text, strip_internal_attrs=strip_internal_attrs), name_map
 
 
 def convert(*args, strip_internal_attrs=False, **kwargs):
     rtlil_text = rtlil.convert(*args, **kwargs)
-    return _convert_rtlil_text(rtlil_text, strip_internal_attrs=strip_internal_attrs)
+    return _convert_rtlil_text_verilog(rtlil_text, strip_internal_attrs=strip_internal_attrs)
