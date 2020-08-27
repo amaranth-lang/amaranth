d12c7827a09daef2389259fc9fbc195454a525aa
whitequark <whitequark@whitequark.org>
2020-08-27 13:39:42 +0000

    setup: synchronize builtin-yosys dependency.

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
     ],
     extras_require={
         # this version requirement needs to be synchronized with the one in nmigen.back.verilog!
-        "builtin-yosys": ["nmigen-yosys>=0.9.*"],
+        "builtin-yosys": ["nmigen-yosys>=0.9.post3527.*"],
         "remote-build": ["paramiko~=2.7"],
     },
     packages=find_packages(),
