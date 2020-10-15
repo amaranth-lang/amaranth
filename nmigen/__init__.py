amaranth-lang/amaranth

commit d71e19e27c2a57037c30b838b13d30426a2173f6

CI: replace Travis with GitHub Actions.
Fixes #445.

.github/workflows/main.yaml
@@ -4,6 +4,37 @@ on:
     types: [opened, reopened, synchronize]
 name: CI
 jobs:
+  test:
+    runs-on: ubuntu-latest
+    strategy:
+      matrix:
+        python-version: ['3.6', '3.7', '3.8', pypy3]
+    steps:
+    - name: Check out source code
+      uses: actions/checkout@v2
+      with:
+        fetch-depth: 0
+    - name: Set up Python
+      uses: actions/setup-python@v2
+      with:
+        python-version: ${{ matrix.python-version }}
+    - name: Install dependencies
+      run: |
+        sudo add-apt-repository ppa:sri-csl/formal-methods
+        sudo apt-get update
+        sudo apt-get install yices2
+        pip install setuptools setuptools_scm wheel coverage codecov yowasp-yosys nmigen-yosys
+        pip install -e .
+    - name: Preserve wasmtime cache
+      uses: actions/cache@v1
+      with:
+        path: ~/.cache/wasmtime
+        key: ${{ runner.os }}-wasmtime
+    - name: Run tests
+      run: |
+        export NMIGEN_USE_YOSYS=builtin YOSYS=yowasp-yosys SBY=yowasp-sby SMTBMC=yowasp-yosys-smtbmc
+        python -m unittest discover
+        codecov
   document:
     runs-on: ubuntu-latest
     steps:

.travis.yml
@@ -1,22 +1,0 @@
-dist: bionic
-language: python
-python:
-  - "3.6"
-  - "3.7"
-  - "3.8"
-  - "pypy3"
-cache:
-  directories:
-    - "$HOME/.local"
-    - "$HOME/.cache/wasmtime"
-before_install:
-  - if ! yices -V; then (git clone https://github.com/SRI-CSL/yices2.git && cd yices2 && autoconf && ./configure --prefix=$HOME/.local && make && make install); fi
-  - pip install coverage codecov pyvcd Jinja2 yowasp-yosys nmigen-yosys
-  - export NMIGEN_USE_YOSYS=builtin YOSYS=yowasp-yosys
-install:
-  - python setup.py develop
-script:
-  - coverage run -m unittest discover
-  - codecov
-matrix:
-  fast_finish: true

commit 80194e1a7ee7d9423a102a8f9f9bc36602d76a2c

CI: fix code coverage collection.
This has been broken since commit d71e19e2 (2020-07-22).

.coveragerc
@@ -4,7 +4,6 @@ include =
   nmigen/*
 omit =
   nmigen/test/*
-  */__init__.py
 
 [report]
 exclude_lines =

.github/workflows/main.yaml
@@ -33,7 +33,7 @@ jobs:
     - name: Run tests
       run: |
         export NMIGEN_USE_YOSYS=builtin YOSYS=yowasp-yosys SBY=yowasp-sby SMTBMC=yowasp-yosys-smtbmc
-        python -m unittest discover
+        python -m coverage run -m unittest
         codecov
   document:
     runs-on: ubuntu-latest

nmigen/__init__.py
@@ -8,7 +8,7 @@ except ImportError:
     # No importlib_metadata. This shouldn't normally happen, but some people prefer not installing
     # packages via pip at all, instead using PYTHONPATH directly or copying the package files into
     # `lib/pythonX.Y/site-packages`. Although not a recommended way, we still try to support it.
-    __version__ = "unknown"
+    __version__ = "unknown" # :nocov:
 
 
 from .hdl import *
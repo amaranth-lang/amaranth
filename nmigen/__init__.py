80194e1a7ee7d9423a102a8f9f9bc36602d76a2c
whitequark <whitequark@whitequark.org>
2020-10-15 18:09:04 +0000

    CI: fix code coverage collection.
    This has been broken since commit d71e19e2 (2020-07-22).

--- nmigen/__init__.py
+++ nmigen/__init__.py
@@ -8,7 +8,7 @@ except ImportError:
     # No importlib_metadata. This shouldn't normally happen, but some people prefer not installing
     # packages via pip at all, instead using PYTHONPATH directly or copying the package files into
     # `lib/pythonX.Y/site-packages`. Although not a recommended way, we still try to support it.
-    __version__ = "unknown"
+    __version__ = "unknown" # :nocov:
 
 
 from .hdl import *
