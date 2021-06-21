44aea951b59380e568f75653129192ddc346c330
Aki Van Ness <aki@lethalbit.net>
2021-06-20 21:25:43 -0400

    examples.vendor: Fixed the tabs in the openlane_asic.py examples so they are spaces now

--- examples/vendor/openlane_asic.py
+++ examples/vendor/openlane_asic.py
@@ -4,39 +4,39 @@ from nmigen.vendor.openlane import *
 import os
 
 class sky130_fd_sc_hd(OpenLANEPlatform):
-	openlane_root = os.environ['OPENLANE_ROOT']
-	pdk = "sky130A"
-	cell_library = "sky130_fd_sc_hd"
+    openlane_root = os.environ['OPENLANE_ROOT']
+    pdk = "sky130A"
+    cell_library = "sky130_fd_sc_hd"
 
-	settings = {
-		"PL_TARGET_DENSITY": 0.75,
-		"FP_HORIZONTAL_HALO": 6,
-		"FP_VERTICAL_HALO": 6,
-		"FP_CORE_UTIL": 5,
-	}
+    settings = {
+        "PL_TARGET_DENSITY": 0.75,
+        "FP_HORIZONTAL_HALO": 6,
+        "FP_VERTICAL_HALO": 6,
+        "FP_CORE_UTIL": 5,
+    }
 
-	connectors  = []
-	resources = []
+    connectors  = []
+    resources = []
 
 
 # An example configurable width inverter for the OpenLANE ASIC flow
 class Inverter(Elaboratable):
-	def __init__(self, width=8):
-		self.i = Signal(width)
-		self.o = Signal(width)
+    def __init__(self, width=8):
+        self.i = Signal(width)
+        self.o = Signal(width)
 
-	def elaborate(self, platform):
-		m = Module()
+    def elaborate(self, platform):
+        m = Module()
 
-		m.d.comb += self.o.eq(~self.i)
+        m.d.comb += self.o.eq(~self.i)
 
-		return m
+        return m
 
 	def get_ports(self):
 		return [self.i, self.o]
 
 if __name__ == "__main__":
-	platform = sky130_fd_sc_hd()
+    platform = sky130_fd_sc_hd()
 
-	inverter = Inverter()
-	platform.build(inverter, name="inverter", ports=inverter.get_ports())
+    # inverter = Inverter()
+    # platform.build(inverter, name="inverter", ports=inverter.get_ports())
