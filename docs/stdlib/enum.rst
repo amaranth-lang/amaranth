f49fee2086d66d590eb4cf910cccfdb01447544b
Wanda <wanda@phinode.net>
2023-11-22 07:35:55 +0100

    Implement RFC 31: Enumeration type safety.

--- docs/stdlib/enum.rst
+++ docs/stdlib/enum.rst
@@ -24,6 +24,8 @@ A shape can be specified for an enumeration with the ``shape=`` keyword argument
 
    >>> Shape.cast(Funct)
    unsigned(4)
+   >>> Value.cast(Funct.ADD)
+   (const 4'd0)
 
 Any :ref:`constant-castable <lang-constcasting>` expression can be used as the value of a member:
 
@@ -57,6 +59,57 @@ The ``shape=`` argument is optional. If not specified, classes from this module 
 
 In this way, this module is a drop-in replacement for the standard :mod:`enum` module, and in an Amaranth project, all ``import enum`` statements may be replaced with ``from amaranth.lib import enum``.
 
+Signals with :class:`Enum` or :class:`Flag` based shape are automatically wrapped in the :class:`EnumView` or :class:`FlagView` value-castable wrappers, which ensure type safety. Any :ref:`value-castable <lang-valuecasting>` can also be explicitly wrapped in a view class by casting it to the enum type:
+
+.. doctest::
+
+   >>> a = Signal(Funct)
+   >>> b = Signal(Op)
+   >>> type(a)
+   <class 'amaranth.lib.enum.EnumView'>
+   >>> a == b
+   Traceback (most recent call last):
+     File "<stdin>", line 1, in <module>
+   TypeError: an EnumView can only be compared to value or other EnumView of the same enum type
+   >>> c = Signal(4)
+   >>> type(Funct(c))
+   <class 'amaranth.lib.enum.EnumView'>
+
+Like the standard Python :class:`enum.IntEnum` and :class:`enum.IntFlag` classes, the Amaranth :class:`IntEnum` and :class:`IntFlag` classes are loosely typed and will not be subject to wrapping in view classes:
+
+.. testcode::
+
+   class TransparentEnum(enum.IntEnum, shape=unsigned(4)):
+       FOO = 0
+       BAR = 1
+
+.. doctest::
+
+   >>> a = Signal(TransparentEnum)
+   >>> type(a)
+   <class 'amaranth.hdl.ast.Signal'>
+
+It is also possible to define a custom view class for a given enum:
+
+.. testcode::
+
+   class InstrView(enum.EnumView):
+       def has_immediate(self):
+           return (self == Instr.ADDI) | (self == Instr.SUBI)
+
+   class Instr(enum.Enum, shape=5, view_class=InstrView):
+       ADD  = Cat(Funct.ADD, Op.REG)
+       ADDI = Cat(Funct.ADD, Op.IMM)
+       SUB  = Cat(Funct.SUB, Op.REG)
+       SUBI = Cat(Funct.SUB, Op.IMM)
+
+.. doctest::
+
+   >>> a = Signal(Instr)
+   >>> type(a)
+   <class 'InstrView'>
+   >>> a.has_immediate()
+   (| (== (sig a) (const 5'd16)) (== (sig a) (const 5'd17)))
 
 Metaclass
 =========
@@ -71,3 +124,9 @@ Base classes
 .. autoclass:: IntEnum()
 .. autoclass:: Flag()
 .. autoclass:: IntFlag()
+
+View classes
+============
+
+.. autoclass:: EnumView()
+.. autoclass:: FlagView()
--- tests/test_lib_enum.py
+++ tests/test_lib_enum.py
@@ -1,7 +1,9 @@
 import enum as py_enum
+import operator
+import sys
 
 from amaranth import *
-from amaranth.lib.enum import Enum, EnumMeta
+from amaranth.lib.enum import Enum, EnumMeta, Flag, IntEnum, EnumView, FlagView
 
 from .utils import *
 
@@ -103,9 +105,9 @@ class EnumTestCase(FHDLTestCase):
         class EnumA(Enum, shape=8):
             Z = 0
             A = 10
-        self.assertRepr(EnumA.const(None), "(const 8'd0)")
-        self.assertRepr(EnumA.const(10), "(const 8'd10)")
-        self.assertRepr(EnumA.const(EnumA.A), "(const 8'd10)")
+        self.assertRepr(EnumA.const(None), "EnumView(EnumA, (const 8'd0))")
+        self.assertRepr(EnumA.const(10), "EnumView(EnumA, (const 8'd10))")
+        self.assertRepr(EnumA.const(EnumA.A), "EnumView(EnumA, (const 8'd10))")
 
     def test_shape_implicit_wrong_in_concat(self):
         class EnumA(Enum):
@@ -118,3 +120,171 @@ class EnumTestCase(FHDLTestCase):
 
     def test_functional(self):
         Enum("FOO", ["BAR", "BAZ"])
+
+    def test_int_enum(self):
+        class EnumA(IntEnum, shape=signed(4)):
+            A = 0
+            B = -3
+        a = Signal(EnumA)
+        self.assertRepr(a, "(sig a)")
+
+    def test_enum_view(self):
+        class EnumA(Enum, shape=signed(4)):
+            A = 0
+            B = -3
+        class EnumB(Enum, shape=signed(4)):
+            C = 0
+            D = 5
+        a = Signal(EnumA)
+        b = Signal(EnumB)
+        c = Signal(EnumA)
+        d = Signal(4)
+        self.assertIsInstance(a, EnumView)
+        self.assertIs(a.shape(), EnumA)
+        self.assertRepr(a, "EnumView(EnumA, (sig a))")
+        self.assertRepr(a.as_value(), "(sig a)")
+        self.assertRepr(a.eq(c), "(eq (sig a) (sig c))")
+        for op in [
+            operator.__add__,
+            operator.__sub__,
+            operator.__mul__,
+            operator.__floordiv__,
+            operator.__mod__,
+            operator.__lshift__,
+            operator.__rshift__,
+            operator.__and__,
+            operator.__or__,
+            operator.__xor__,
+            operator.__lt__,
+            operator.__le__,
+            operator.__gt__,
+            operator.__ge__,
+        ]:
+            with self.assertRaises(TypeError):
+                op(a, a)
+            with self.assertRaises(TypeError):
+                op(a, d)
+            with self.assertRaises(TypeError):
+                op(d, a)
+            with self.assertRaises(TypeError):
+                op(a, 3)
+            with self.assertRaises(TypeError):
+                op(a, EnumA.A)
+        for op in [
+            operator.__eq__,
+            operator.__ne__,
+        ]:
+            with self.assertRaises(TypeError):
+                op(a, b)
+            with self.assertRaises(TypeError):
+                op(a, d)
+            with self.assertRaises(TypeError):
+                op(d, a)
+            with self.assertRaises(TypeError):
+                op(a, 3)
+            with self.assertRaises(TypeError):
+                op(a, EnumB.C)
+        self.assertRepr(a == c, "(== (sig a) (sig c))")
+        self.assertRepr(a != c, "(!= (sig a) (sig c))")
+        self.assertRepr(a == EnumA.B, "(== (sig a) (const 4'sd-3))")
+        self.assertRepr(EnumA.B == a, "(== (sig a) (const 4'sd-3))")
+        self.assertRepr(a != EnumA.B, "(!= (sig a) (const 4'sd-3))")
+
+    def test_flag_view(self):
+        class FlagA(Flag, shape=unsigned(4)):
+            A = 1
+            B = 4
+        class FlagB(Flag, shape=unsigned(4)):
+            C = 1
+            D = 2
+        a = Signal(FlagA)
+        b = Signal(FlagB)
+        c = Signal(FlagA)
+        d = Signal(4)
+        self.assertIsInstance(a, FlagView)
+        self.assertRepr(a, "FlagView(FlagA, (sig a))")
+        for op in [
+            operator.__add__,
+            operator.__sub__,
+            operator.__mul__,
+            operator.__floordiv__,
+            operator.__mod__,
+            operator.__lshift__,
+            operator.__rshift__,
+            operator.__lt__,
+            operator.__le__,
+            operator.__gt__,
+            operator.__ge__,
+        ]:
+            with self.assertRaises(TypeError):
+                op(a, a)
+            with self.assertRaises(TypeError):
+                op(a, d)
+            with self.assertRaises(TypeError):
+                op(d, a)
+            with self.assertRaises(TypeError):
+                op(a, 3)
+            with self.assertRaises(TypeError):
+                op(a, FlagA.A)
+        for op in [
+            operator.__eq__,
+            operator.__ne__,
+            operator.__and__,
+            operator.__or__,
+            operator.__xor__,
+        ]:
+            with self.assertRaises(TypeError):
+                op(a, b)
+            with self.assertRaises(TypeError):
+                op(a, d)
+            with self.assertRaises(TypeError):
+                op(d, a)
+            with self.assertRaises(TypeError):
+                op(a, 3)
+            with self.assertRaises(TypeError):
+                op(a, FlagB.C)
+        self.assertRepr(a == c, "(== (sig a) (sig c))")
+        self.assertRepr(a != c, "(!= (sig a) (sig c))")
+        self.assertRepr(a == FlagA.B, "(== (sig a) (const 4'd4))")
+        self.assertRepr(FlagA.B == a, "(== (sig a) (const 4'd4))")
+        self.assertRepr(a != FlagA.B, "(!= (sig a) (const 4'd4))")
+        self.assertRepr(a | c, "FlagView(FlagA, (| (sig a) (sig c)))")
+        self.assertRepr(a & c, "FlagView(FlagA, (& (sig a) (sig c)))")
+        self.assertRepr(a ^ c, "FlagView(FlagA, (^ (sig a) (sig c)))")
+        self.assertRepr(~a, "FlagView(FlagA, (& (~ (sig a)) (const 3'd5)))")
+        self.assertRepr(a | FlagA.B, "FlagView(FlagA, (| (sig a) (const 4'd4)))")
+        if sys.version_info >= (3, 11):
+            class FlagC(Flag, shape=unsigned(4), boundary=py_enum.KEEP):
+                A = 1
+                B = 4
+            e = Signal(FlagC)
+            self.assertRepr(~e, "FlagView(FlagC, (~ (sig e)))")
+
+    def test_enum_view_wrong(self):
+        class EnumA(Enum, shape=signed(4)):
+            A = 0
+            B = -3
+
+        a = Signal(2)
+        with self.assertRaisesRegex(TypeError,
+                r'^EnumView target must have the same shape as the enum$'):
+            EnumA(a)
+        with self.assertRaisesRegex(TypeError,
+                r'^EnumView target must be a value-castable object, not .*$'):
+            EnumView(EnumA, "a")
+
+        class EnumB(Enum):
+            C = 0
+            D = 1
+        with self.assertRaisesRegex(TypeError,
+                r'^EnumView type must be an enum with shape, not .*$'):
+            EnumView(EnumB, 3)
+
+    def test_enum_view_custom(self):
+        class CustomView(EnumView):
+            pass
+        class EnumA(Enum, view_class=CustomView, shape=unsigned(2)):
+            A = 0
+            B = 1
+        a = Signal(EnumA)
+        assert isinstance(a, CustomView)
