7db049f37f675758c08528277c5222cadb7ba9a9
Catherine <whitequark@whitequark.org>
2023-12-07 21:03:39 +0000

    Remove remaining traces of `$verilog_initial_trigger`.
    This construct was originally removed in commit b452e0e8. It has not been relevant since Yosys 0.10.

--- amaranth/vendor/_lattice_ice40.py
+++ amaranth/vendor/_lattice_ice40.py
@@ -119,7 +119,6 @@ class LatticeICE40Platform(TemplatedPlatform):
                 read_ilang {{file}}
             {% endfor %}
             read_ilang {{name}}.il
-            delete w:$verilog_initial_trigger
             {{get_override("script_after_read")|default("# (script_after_read placeholder)")}}
             synth_ice40 {{get_override("synth_opts")|options}} -top {{name}}
             {{get_override("script_after_synth")|default("# (script_after_synth placeholder)")}}
