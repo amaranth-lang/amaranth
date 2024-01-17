fcddff744031fa0fea1271166e6d5e9e5ffcd57e
Catherine <whitequark@whitequark.org>
2024-01-17 17:49:50 +0000

    docs: update cover, title, and copyright.

--- docs/conf.py
+++ docs/conf.py
@@ -1,12 +1,13 @@
 import os, sys
 sys.path.insert(0, os.path.abspath("."))
 
+import time
 import amaranth
 
-project = "Amaranth HDL toolchain"
+project = "Amaranth language & toolchain"
 version = amaranth.__version__.replace(".editable", "")
 release = version.split("+")[0]
-copyright = "2020—2023, Amaranth HDL developers"
+copyright = time.strftime("2020—%Y, Amaranth project contributors")
 
 extensions = [
 	"sphinx.ext.intersphinx",
