amaranth-lang/amaranth

commit 78b90fbafafc176dbcc52858ae5b1b15bdeb1fe2

build.plat,vendor: fix toolchain environment variable check.
The bug was introduced in commit 15b6068c. A changelog entry was also
missing.

Fixes #1089.

amaranth/build/plat.py
@@ -89,8 +89,7 @@ class Platform(ResourceManager, metaclass=ABCMeta):
         #     may fail.
         # This is OK because even if `require_tool` succeeds, the toolchain might be broken anyway.
         # The check only serves to catch common errors earlier.
-        got_env_var = any(v in os.environ for v in self._all_toolchain_env_vars)
-        if do_build and not got_env_var:
+        if do_build and not self._toolchain_env_var not in os.environ:
             for tool in self.required_tools:
                 require_tool(tool)
 
@@ -105,7 +104,7 @@ class Platform(ResourceManager, metaclass=ABCMeta):
         self.toolchain_program(products, name, **(program_opts or {}))
 
     def has_required_tools(self):
-        if any(v in os.environ for v in self._all_toolchain_env_vars):
+        if self._toolchain_env_var in os.environ:
             return True
         return all(has_tool(name) for name in self.required_tools)
 
@@ -271,17 +270,13 @@ class TemplatedPlatform(Platform):
             #!/bin/sh
             # {{autogenerated}}
             set -e{{verbose("x")}}
-            {% for var in platform._all_toolchain_env_vars %}
-            [ -n "${{var}}" ] && . "${{var}}"
-            {% endfor %}
+            [ -n "${{platform._toolchain_env_var}}" ] && . "${{platform._toolchain_env_var}}"
             {{emit_commands("sh")}}
         """,
         "build_{{name}}.bat": """
             @rem {{autogenerated}}
             {{quiet("@echo off")}}
-            {% for var in platform._all_toolchain_env_vars %}
-            if defined {{var}} call %{{var}}%
-            {% endfor %}
+            if defined {{platform._toolchain_env_var}} call %{{platform._toolchain_env_var}}%
             {{emit_commands("bat")}}
         """,
     }

amaranth/vendor/_intel.py
@@ -82,13 +82,11 @@ class IntelPlatform(TemplatedPlatform):
         "build_{{name}}.sh": r"""
             #!/bin/sh
             # {{autogenerated}}
-            {% for var in platform._all_toolchain_env_vars %}
-            if [ -n "${{var}}" ]; then
-                QUARTUS_ROOTDIR=$(dirname $(dirname "${{var}}"))
+            if [ -n "${{platform._toolchain_env_var}}" ]; then
+                QUARTUS_ROOTDIR=$(dirname $(dirname "${{platform._toolchain_env_var}}"))
                 # Quartus' qenv.sh does not work with `set -e`.
-                . "${{var}}"
+                . "${{platform._toolchain_env_var}}"
             fi
-            {% endfor %}
             set -e{{verbose("x")}}
             {{emit_commands("sh")}}
         """,

amaranth/vendor/_lattice_ecp5.py
@@ -186,12 +186,10 @@ class LatticeECP5Platform(TemplatedPlatform):
             # {{autogenerated}}
             set -e{{verbose("x")}}
             if [ -z "$BASH" ] ; then exec /bin/bash "$0" "$@"; fi
-            {% for var in platform._all_toolchain_env_vars %}
-            if [ -n "${{var}}" ]; then
-                bindir=$(dirname "${{var}}")
-                . "${{var}}"
+            if [ -n "${{platform._toolchain_env_var}}" ]; then
+                bindir=$(dirname "${{platform._toolchain_env_var}}")
+                . "${{platform._toolchain_env_var}}"
             fi
-            {% endfor %}
             {{emit_commands("sh")}}
         """,
         "{{name}}.v": r"""

amaranth/vendor/_lattice_ice40.py
@@ -178,22 +178,16 @@ class LatticeICE40Platform(TemplatedPlatform):
             #!/bin/sh
             # {{autogenerated}}
             set -e{{verbose("x")}}
-            {% for var in platform._all_toolchain_env_vars %}
-            if [ -n "${{var}}" ]; then
-                # LSE environment
-                export LD_LIBRARY_PATH=${{var}}/LSE/bin/lin64:$LD_LIBRARY_PATH
-                export PATH=${{var}}/LSE/bin/lin64:$PATH
-                export FOUNDRY=${{var}}/LSE
-                # Synplify environment
-                export LD_LIBRARY_PATH=${{var}}/sbt_backend/bin/linux/opt/synpwrap:$LD_LIBRARY_PATH
-                export PATH=${{var}}/sbt_backend/bin/linux/opt/synpwrap:$PATH
-                export SYNPLIFY_PATH=${{var}}/synpbase
-                # Common environment
-                export SBT_DIR=${{var}}/sbt_backend
-            else
-                echo "Variable ${{platform._toolchain_env_var}} must be set" >&2; exit 1
-            fi
-            {% endfor %}
+            # LSE environment
+            export LD_LIBRARY_PATH=${{platform._toolchain_env_var}}/LSE/bin/lin64:$LD_LIBRARY_PATH
+            export PATH=${{platform._toolchain_env_var}}/LSE/bin/lin64:$PATH
+            export FOUNDRY=${{platform._toolchain_env_var}}/LSE
+            # Synplify environment
+            export LD_LIBRARY_PATH=${{platform._toolchain_env_var}}/sbt_backend/bin/linux/opt/synpwrap:$LD_LIBRARY_PATH
+            export PATH=${{platform._toolchain_env_var}}/sbt_backend/bin/linux/opt/synpwrap:$PATH
+            export SYNPLIFY_PATH=${{platform._toolchain_env_var}}/synpbase
+            # Common environment
+            export SBT_DIR=${{platform._toolchain_env_var}}/sbt_backend
             {{emit_commands("sh")}}
         """,
         "{{name}}.v": r"""

amaranth/vendor/_lattice_machxo_2_3l.py
@@ -52,12 +52,10 @@ class LatticeMachXO2Or3LPlatform(TemplatedPlatform):
             # {{autogenerated}}
             set -e{{verbose("x")}}
             if [ -z "$BASH" ] ; then exec /bin/bash "$0" "$@"; fi
-            {% for var in platform._all_toolchain_env_vars %}
-            if [ -n "${{var}}" ]; then
-                bindir=$(dirname "${{var}}")
-                . "${{var}}"
+            if [ -n "${{platform._toolchain_env_var}}" ]; then
+                bindir=$(dirname "${{platform._toolchain_env_var}}")
+                . "${{platform._toolchain_env_var}}"
             fi
-            {% endfor %}
             {{emit_commands("sh")}}
         """,
         "{{name}}.v": r"""

amaranth/vendor/_xilinx.py
@@ -143,9 +143,7 @@ class XilinxPlatform(TemplatedPlatform):
             # {{autogenerated}}
             set -e{{verbose("x")}}
             if [ -z "$BASH" ] ; then exec /bin/bash "$0" "$@"; fi
-            {% for var in platform._all_toolchain_env_vars %}
-            [ -n "${{var}}" ] && . "${{var}}"
-            {% endfor %}
+            [ -n "${{platform._toolchain_env_var}}" ] && . "${{platform._toolchain_env_var}}"
             {{emit_commands("sh")}}
         """,
         "{{name}}.v": r"""
@@ -261,9 +259,7 @@ class XilinxPlatform(TemplatedPlatform):
             # {{autogenerated}}
             set -e{{verbose("x")}}
             if [ -z "$BASH" ] ; then exec /bin/bash "$0" "$@"; fi
-            {% for var in platform._all_toolchain_env_vars %}
-            [ -n "${{var}}" ] && . "${{var}}"
-            {% endfor %}
+            [ -n "${{platform._toolchain_env_var}}" ] && . "${{platform._toolchain_env_var}}"
             {{emit_commands("sh")}}
         """,
         "{{name}}.v": r"""
@@ -516,9 +512,7 @@ class XilinxPlatform(TemplatedPlatform):
         "build_{{name}}.sh": r"""
             # {{autogenerated}}
             set -e{{verbose("x")}}
-            {% for var in platform._all_toolchain_env_vars %}
-            [ -n "${{var}}" ] && . "${{var}}"
-            {% endfor %}
+            [ -n "${{platform._toolchain_env_var}}" ] && . "${{platform._toolchain_env_var}}"
             : ${DB_DIR:=/usr/share/nextpnr/prjxray-db}
             : ${CHIPDB_DIR:=/usr/share/nextpnr/xilinx-chipdb}
             {{emit_commands("sh")}}

commit 5fbab06de8fbf6b8683a066063fc7268dd029162

build.plat: fix toolchain environment variable check, #2.
Fixes typo introduced in commit 78b90fba.

amaranth/build/plat.py
@@ -89,7 +89,7 @@ class Platform(ResourceManager, metaclass=ABCMeta):
         #     may fail.
         # This is OK because even if `require_tool` succeeds, the toolchain might be broken anyway.
         # The check only serves to catch common errors earlier.
-        if do_build and not self._toolchain_env_var not in os.environ:
+        if do_build and self._toolchain_env_var not in os.environ:
             for tool in self.required_tools:
                 require_tool(tool)
 