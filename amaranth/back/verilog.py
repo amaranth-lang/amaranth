2ab3a4a0a05db230ae23e58f5e30bab2cb804aef
Catherine <whitequark@whitequark.org>
2024-03-22 00:57:48 +0000

    docs/install: fix Yosys version requirement.

--- amaranth/back/verilog.py
+++ amaranth/back/verilog.py
@@ -8,7 +8,7 @@ __all__ = ["YosysError", "convert", "convert_fragment"]
 
 
 def _convert_rtlil_text(rtlil_text, *, strip_internal_attrs=False, write_verilog_opts=()):
-    # this version requirement needs to be synchronized with the one in pyproject.toml!
+    # This version requirement needs to be synchronized with the one in pyproject.toml!
     yosys = find_yosys(lambda ver: ver >= (0, 38))
 
     script = []
