09045a2724cee50f4d285066e8018d3a882d1785
Wanda <wanda@phinode.net>
2024-04-18 16:44:41 +0200

    vendor._lattice: merge ECP5 and MachXO[23] into one platform.

--- amaranth/vendor/__init__.py
+++ amaranth/vendor/__init__.py
@@ -11,6 +11,7 @@ __all__ = [
     "LatticeICE40Platform",
     "LatticeMachXO2Platform",
     "LatticeMachXO3LPlatform",
+    "LatticePlatform",
     "QuicklogicPlatform",
     "SiliconBluePlatform",
     "XilinxPlatform",
@@ -28,12 +29,10 @@ def __getattr__(name):
     if name == "GowinPlatform":
         from ._gowin import GowinPlatform
         return GowinPlatform
-    if name == "LatticeECP5Platform":
-        from ._lattice_ecp5 import LatticeECP5Platform
-        return LatticeECP5Platform
-    if name in ("LatticeMachXO2Platform", "LatticeMachXO3LPlatform"):
-        from ._lattice_machxo_2_3l import LatticeMachXO2Or3LPlatform
-        return LatticeMachXO2Or3LPlatform
+    if name in ("LatticePlatform", "LatticeECP5Platform", "LatticeMachXO2Platform",
+                "LatticeMachXO3LPlatform"):
+        from ._lattice import LatticePlatform
+        return LatticePlatform
     if name == "QuicklogicPlatform":
         from ._quicklogic import QuicklogicPlatform
         return QuicklogicPlatform
