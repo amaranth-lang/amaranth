bf98136e2101d62aabfe2d1274bde87bb9dcd497
Catherine <whitequark@whitequark.org>
2024-07-22 17:53:14 +0000

    Implement RFC 17: Remove `log2_int`.

--- amaranth/utils.py
+++ amaranth/utils.py
@@ -3,7 +3,7 @@ import operator
 from ._utils import deprecated
 
 
-__all__ = ["ceil_log2", "exact_log2", "log2_int", "bits_for"]
+__all__ = ["ceil_log2", "exact_log2", "bits_for"]
 
 
 def ceil_log2(n):
@@ -28,17 +28,6 @@ def exact_log2(n):
     if n <= 0 or (n & (n - 1)):
         raise ValueError(f"{n} is not a power of 2")
     return (n - 1).bit_length()
-
-
-@deprecated("instead of `log2_int(n, True)`, use `exact_log2(n)`; instead of `log2_int(n, False)` use `ceil_log2(n)`")
-def log2_int(n, need_pow2=True):
-    n = operator.index(n)
-    if n == 0:
-        return 0
-    r = (n - 1).bit_length()
-    if need_pow2 and (1 << r) != n:
-        raise ValueError(f"{n} is not a power of 2")
-    return r
 
 
 def bits_for(n, require_sign_bit=False):
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -30,26 +30,6 @@ class Log2TestCase(unittest.TestCase):
         with self.assertRaises(TypeError):
             exact_log2(1.5)
 
-    @_ignore_deprecated
-    def test_log2_int(self):
-        self.assertEqual(log2_int(1), 0)
-        self.assertEqual(log2_int(2), 1)
-        self.assertEqual(log2_int(4), 2)
-        self.assertEqual(log2_int(8), 3)
-        for val in [-1, 3, 5, 6, 7, 9]:
-            with self.assertRaises(ValueError):
-                log2_int(val)
-        with self.assertRaises(TypeError):
-            log2_int(1.5)
-        self.assertEqual(log2_int(0, False), 0)
-        self.assertEqual(log2_int(1, False), 0)
-        self.assertEqual(log2_int(2, False), 1)
-        self.assertEqual(log2_int(3, False), 2)
-        self.assertEqual(log2_int(4, False), 2)
-        self.assertEqual(log2_int(5, False), 3)
-        self.assertEqual(log2_int(8, False), 3)
-        self.assertEqual(log2_int(9, False), 4)
-
     def test_bits_for(self):
         self.assertEqual(bits_for(-4), 3)
         self.assertEqual(bits_for(-3), 3)
