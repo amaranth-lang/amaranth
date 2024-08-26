70f5acb710ad6b614d54d9ae0b7110c29bed3e46
Wanda <wanda@phinode.net>
2024-08-26 23:39:40 +0200

    hdl.mem: mask initial value to shape.
    Fixes #1492.

--- tests/test_hdl_mem.py
+++ tests/test_hdl_mem.py
@@ -33,6 +33,33 @@ class MemoryDataTestCase(FHDLTestCase):
                 r"^Value \(memory-row \(memory-data data\) 0\) can only be used in simulator processes$"):
             m.d.comb += data[0].eq(1)
 
+
+class InitTestCase(FHDLTestCase):
+    def test_ones(self):
+        init = MemoryData.Init([-1, 12], shape=8, depth=2)
+        self.assertEqual(list(init), [0xff, 12])
+        init = MemoryData.Init([-1, -12], shape=signed(8), depth=2)
+        self.assertEqual(list(init), [-1, -12])
+
+    def test_trunc(self):
+        with self.assertWarnsRegex(SyntaxWarning,
+                r"^Initial value -2 is signed, but the memory shape is unsigned\(8\)$"):
+            init = MemoryData.Init([-2, 12], shape=8, depth=2)
+        self.assertEqual(list(init), [0xfe, 12])
+        with self.assertWarnsRegex(SyntaxWarning,
+                r"^Initial value 258 will be truncated to the memory shape unsigned\(8\)$"):
+            init = MemoryData.Init([258, 129], shape=8, depth=2)
+        self.assertEqual(list(init), [2, 129])
+        with self.assertWarnsRegex(SyntaxWarning,
+                r"^Initial value 128 will be truncated to the memory shape signed\(8\)$"):
+            init = MemoryData.Init([128], shape=signed(8), depth=1)
+        self.assertEqual(list(init), [-128])
+        with self.assertWarnsRegex(SyntaxWarning,
+                r"^Initial value -129 will be truncated to the memory shape signed\(8\)$"):
+            init = MemoryData.Init([-129], shape=signed(8), depth=1)
+        self.assertEqual(list(init), [127])
+
+
 class MemoryTestCase(FHDLTestCase):
     def test_name(self):
         with _ignore_deprecated():
@@ -73,7 +100,7 @@ class MemoryTestCase(FHDLTestCase):
         with _ignore_deprecated():
             with self.assertRaisesRegex(TypeError,
                     (r"^Memory initialization value at address 1: "
-                        r"'str' object cannot be interpreted as an integer$")):
+                        r"Initial value must be a constant-castable expression, not '0'$")):
                 m = Memory(width=8, depth=4, init=[1, "0"])
 
     def test_attrs(self):
