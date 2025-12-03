53d88ad1bab4d54dc11a1be57081200bcef766ae
miyuko <miyuko@calayucu.com>
2025-12-03 03:25:13 +0000

    docs: add a version switch.

--- docs/conf.py
+++ docs/conf.py
@@ -57,6 +57,7 @@ napoleon_custom_sections = [
 html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
 html_css_files = ["custom.css"]
+html_js_files = ["version-switch.js"]
 html_logo = "_static/logo.png"
 
 rst_prolog = """
