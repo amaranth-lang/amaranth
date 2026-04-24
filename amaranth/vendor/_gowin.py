ab08361a0b8652c852535dbfe8d4ed04755508eb
Jean THOMAS <jean@lambdaconcept.com>
2026-04-24 12:25:14 +0200

    gowin: Update subseries as of IDE V1.9.12.02_SP2
    Add D, ES, P5B, P5C, SA subseries. Subseries were also sorted in
    alphabetical order.

--- amaranth/vendor/_gowin.py
+++ amaranth/vendor/_gowin.py
@@ -205,7 +205,7 @@ class GowinPlatform(TemplatedPlatform):
         reg_series    = r"(GW[1235]{1}[AN]{1}[EFNRSZT]{0,3})-"
         reg_voltage   = r"(ZV|EV|LV|LX|UV|UX)"
         reg_size      = r"(1|2|3|4|5|9|15|18|20|25|55|60|75|138)"
-        reg_subseries = r"(?:(A|B|C|S|X|P5)?)"
+        reg_subseries = r"(?:(A|B|C|D|ES|P5|P5B|P5C|S|SA|X)?)"
         reg_package   = r"((?:FPG|PG|UG|EQ|LQ|MG|M|QN|CS|FN)(?:\d+)(?:P?)(?:A|E|M|CF|C|D|G|H|F|S|T|U|X)?)"
         reg_speed     = r"((?:C\d{1}/I\d{1})|ES|A\d{1}|I\d{1})"
 
